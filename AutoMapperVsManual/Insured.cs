f56f84b2603cfe1e13cfff36831f5bf95f22f06b
Egor <sychyov1991@mail.ru>
2021-12-28 19:29:14 +0600

    Add project files.

---
+++ AutoMapperVsManual/Insured.cs
@@ -0,0 +1,28 @@
+﻿namespace AutoMapperVsManual
+{
+    public class Insured
+    {
+        public int Id { get; set; }
+        public int DriveClass { get; set; }
+
+        
+    }
+
+    public class Driver
+    {
+        public int Id { get; set; }
+        public int DriveClass { get; set; }
+    }
+
+    public static class Maping
+    {
+        public static Insured ToInsured(this Driver driver)
+        {
+            return new Insured
+            {
+                Id = driver.Id,
+                DriveClass = driver.DriveClass,
+            };
+        }
+    }
+}
---
+++ AutoMapperVsManual/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AutoMapper;
+using AutoMapperVsManual;
+
+Console.WriteLine("Hello, World!");
+
+var config = new MapperConfiguration(cfg => {
+    cfg.CreateMap<Driver, Insured>();
+});
+
+IMapper mapper = config.CreateMapper();
+
+
+var driver = new Driver { Id = 1, DriveClass = 2};
+
+
+var insured = driver.ToInsured();
+var insuredAM = mapper.Map<Driver, Insured>(driver);
+
+Console.WriteLine(insuredAM);
