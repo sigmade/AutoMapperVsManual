2254d0943fdbf781279491802a4c4fe724718376
Egor <sychyov1991@mail.ru>
2022-01-18 21:06:01 +0600

    style change

--- AutoMapperVsManual/Program.cs
+++ AutoMapperVsManual/Program.cs
@@ -16,15 +16,13 @@ timer.Start();
 var dest = source.ToDest();
 timer.Stop();
 TimeSpan timeSpan = timer.Elapsed;
-Console.WriteLine($"StaticMethod {timeSpan:s\\.ffff} sec");
+Console.WriteLine($"  StaticMethod {timeSpan:s\\.ffff} sec");
 
 timer.Restart();
-
 
 var destAM = mapper.Map<Source, Dest>(source);
 timer.Stop();
 TimeSpan timeSpan2 = timer.Elapsed;
-
-Console.WriteLine($"Automapper {timeSpan2:s\\.ffff} sec");
+Console.WriteLine($"  Automapper {timeSpan2:s\\.ffff} sec");
 
 Console.ReadKey();
